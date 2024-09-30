7b47b84807f510ace40c782cfcc19e3eaa5e4884
Olalekan Sanni <100454673+1MAGNOVA@users.noreply.github.com>
2024-09-30 21:06:31 +0100

    Revert "No longer using Python Google sheet library "

---
+++ main.py
@@ -0,0 +1,8 @@
+""" Import Dependencies"""
+
+import pandas as pd
+import requests 
+import gsheets as Sheets
+from google.oauth2 import service_account as s_a 
+from googleapiclient.discovery import build 
+
